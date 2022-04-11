470c9b11fc2980951c99667a46cb26be446c01c3
naihe138 <370215230@qq.com>
2022-04-11 22:14:07 +0800

    cheasd

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,5 +1,5 @@
 import { AxiosRequestConfig } from 'axios';
-import { ref } from 'vue';
+import { ref, UnwrapRef, Ref } from 'vue';
 import { http } from '../utils/http';
 
 export type UseRequestOptions = {
@@ -17,16 +17,15 @@ export function useRequest<Q = Recordable, R = Recordable>(
   const response = ref<R | null>(null);
   const error = ref<Error | null>(null);
   const loading = ref(false);
-
   const { immediate, formatResult } = config || {};
-  // debugger
-  const run = () => {
+  const run = (requestBody: Recordable = {}) => {
     loading.value = true;
     const rConfig = (config || {}) as AxiosRequestConfig;
-    const ret = http(url, data, rConfig);
+    const mergeBody = { ...data, ...requestBody };
+    const ret = http(url, mergeBody, rConfig);
     return ret
-      .then((resp: any) => {
-        const result = formatResult ? formatResult(resp) : resp;
+      .then((res) => {
+        const result = formatResult ? formatResult(res) : res;
         loading.value = false;
         error.value = null;
         response.value = result;
@@ -42,5 +41,6 @@ export function useRequest<Q = Recordable, R = Recordable>(
   if (immediate) {
     run();
   }
-  return { data: response, error, loading, run };
+  const _response = response as Ref<UnwrapRef<R>>;
+  return { data: _response, error, loading, run };
 }
